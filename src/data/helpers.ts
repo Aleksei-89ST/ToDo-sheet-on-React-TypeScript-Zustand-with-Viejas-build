b8f37cdfdecd0f43e5e2ebf6e20c128aa95c345c
Aleksei-89ST <leha.leonov.52@bk.ru>
2022-05-02 00:03:13 +0400

    создал помошник для генерации с id

---
+++ src/data/helpers.ts
@@ -0,0 +1,5 @@
+//помошник для генерации с id
+type GenerateId = () => string;
+export const generateId: GenerateId = () => (
+    Math.random().toString(16).slice(2) + new Date().getTime().toString(36)
+);
