fca3e2a06aa6204922f7baed2fbca28b199bd44e
Aleksei-89ST <leha.leonov.52@bk.ru>
2022-05-02 13:38:43 +0400

    описал стили к inputPlus

--- src/data/stores/useToDoStore.ts
+++ src/data/stores/useToDoStore.ts
@@ -1,36 +1,54 @@
-import create from 'zustand'
-import {generateId} from '../helpers'
-
+import create from "zustand";
+import { generateId } from "../helpers";
 
 // тут мы определяем 2 интерфейса
 interface Task {
-    id:string;
-    title:string;
-    createdAt:Number;
+  id: string;
+  title: string;
+  createdAt: number;
 }
 
 interface ToDoStore {
-    tasks:[];
-    createTask:(title:string) => void;
-    updateTask:(id:string,title:string) => void;
-    removeTask:(id:string) => void;
+  tasks: Task[];
+  createTask: (title: string) => void;
+  updateTask: (id: string, title: string) => void;
+  removeTask: (id: string) => void;
 }
 // создали store
-export const useToDoStore = create<ToDoStore>((set,get) => ({
-tasks:[],
-createTask:(title) => {
-    const {tasks} = get();
+export const useToDoStore = create<ToDoStore>((set, get) => ({
+  tasks: [
+      {
+          id:'hsidvdvdfbvfb',
+          title:'Моя дефолтная таска',
+          createdAt:232342
+      },
+  ],
+  createTask: (title) => {
+    const { tasks } = get();
     //создаю новую задачу и соответственно новый state
- const newTask = {
-     id:generateId(),
-     title,
-     createdAt:Date.now(),
- }
- set({
-     // беру массив элементов и присоединяю с помощью concat задачи
-     tasks:[newTask].concat(tasks),
- })
-},
-updateTask:(id,title) => {},
-removeTask:(id) => {},
-}))
+    const newTask = {
+      id: generateId(),
+      title,
+      createdAt: Date.now(),
+    }
+    // беру массив элементов и присоединяю с помощью concat задачи
+    set({
+      tasks: [newTask].concat(tasks),
+    })
+  },
+  updateTask: (id: string, title: string) => {
+    const { tasks } = get();
+    set({
+      tasks: tasks.map((task) => ({
+        ...task,
+        title: task.id === id ? title : task.title,
+      }))
+    });
+  },
+  removeTask: (id:string) => {
+    const { tasks } = get();
+    set({
+      tasks: tasks.filter((task) => task.id !== id )
+    });
+  },
+}));
