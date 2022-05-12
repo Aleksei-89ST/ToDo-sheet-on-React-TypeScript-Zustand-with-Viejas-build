fd7cda424cb843f6e6971245843bf6725c6705bd
Aleksei-89ST <leha.leonov.52@bk.ru>
2022-05-12 14:27:38 +0400

    ToDo alredy

--- src/data/stores/useToDoStore.ts
+++ src/data/stores/useToDoStore.ts
@@ -1,4 +1,5 @@
-import create from "zustand";
+import create, { State, StateCreator } from "zustand";
+import { devtools } from "zustand/middleware";
 import { generateId } from "../helpers";
 
 // тут мы определяем 2 интерфейса
@@ -14,46 +15,69 @@ interface ToDoStore {
   updateTask: (id: string, title: string) => void;
   removeTask: (id: string) => void;
 }
-// создали store
-export const useToDoStore = create<ToDoStore>((set, get) => ({
-  tasks: [
-      {
-          id:'hsidvdvdfbvfb',
-          title:'Моя дефолтная таска',
-          createdAt:232342
+function isToDoStore(object: any): object is ToDoStore {
+  return "tasks" in object;
+}
+
+const lokalStorageUpdate =
+  <T extends State>(config: StateCreator<T>): StateCreator<T> =>
+  (set, get, api) =>
+    config(
+      (nextState, ...args) => {
+        if (isToDoStore(nextState)) {
+          window.localStorage.setItem(
+            "tasks",
+            JSON.stringify(nextState.tasks)
+          );
+        }
+        set(nextState, ...args) // это мидлвара оперирунт на более верхнем уровне над нашим state
       },
-      {
-        id:'hsidvdvdfbv',
-        title:'Моя дефолтная таска2',
-        createdAt:232342
+      get,
+      api
+    );
+    const getCurrentState = () => {
+      try {
+        const currentState = (JSON.parse(window.localStorage.getItem('tasks') || 
+        '[]')) as Task[];
+        return currentState
+      } catch (err) {
+        window.localStorage.setItem(
+          "tasks",'[]');
+      }
+      return [];
+    } 
+     
+// создали store
+export const useToDoStore = create<ToDoStore>(lokalStorageUpdate
+(devtools((set, get) => ({
+    tasks: getCurrentState(),
+    createTask: (title) => {
+      const { tasks } = get();
+      //создаю новую задачу и соответственно новый state
+      const newTask = {
+        id: generateId(),
+        title,
+        createdAt: Date.now(),
+      };
+      // беру массив элементов и присоединяю с помощью concat задачи
+      set({
+        tasks: [newTask].concat(tasks),
+      });
     },
-  ],
-  createTask: (title) => {
-    const { tasks } = get();
-    //создаю новую задачу и соответственно новый state
-    const newTask = {
-      id: generateId(),
-      title,
-      createdAt: Date.now(),
-    }
-    // беру массив элементов и присоединяю с помощью concat задачи
-    set({
-      tasks: [newTask].concat(tasks),
-    })
-  },
-  updateTask: (id: string, title: string) => {
-    const { tasks } = get();
-    set({
-      tasks: tasks.map((task) => ({
-        ...task,
-        title: task.id === id ? title : task.title,
-      }))
-    });
-  },
-  removeTask: (id:string) => {
-    const { tasks } = get();
-    set({
-      tasks: tasks.filter((task) => task.id !== id )
-    });
-  },
-}));
+    updateTask: (id: string, title: string) => {
+      const { tasks } = get();
+      set({
+        tasks: tasks.map((task) => ({
+          ...task,
+          title: task.id === id ? title : task.title,
+        })),
+      });
+    },
+    removeTask: (id: string) => {
+      const { tasks } = get();
+      set({
+        tasks: tasks.filter((task) => task.id !== id),
+      });
+    },
+  }))
+));
